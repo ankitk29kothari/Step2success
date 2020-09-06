5591a051b5d1f26b0e8293bae272a88715fe381e
ankitk29kothari <49672664+ankitk29kothari@users.noreply.github.com>
2020-09-06 17:02:09 +0530

    v

--- Python/Regex/Regex_basicsEx.py
+++ Python/Regex/Regex_basicsEx.py
@@ -3,14 +3,14 @@ import re
 text='My name is ankit Kothari & ankit \
 is working in Obs.maattres My phone no is 808-266-132A and 907.432.4512 cat, bat, mat, lat'
 
-text2='t ha@ hahahaha2 hadfdfd'
+text2='SRT\SRT 359891 Quote 455  Burgos Aranda spain  MLAN Maintenance change_55 form.xlsx'
 
 pattern=re.compile(r'[897]0\d[.-]\d{3}[.-]\d*')
-pattern1=re.compile(r'\b[^b]at\w*')
-matches=pattern1.finditer(text)
 
-for i in matches:
-	print(i)
+pattern1=re.compile(r'\w\w\w\w?\w?.\d\d\d\d?\d?\d?')
+matches=pattern1.findall(text2)
+matches_str=' '.join(matches)
+print(matches_str)
 
 
 
